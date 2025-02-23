8964c705829f7eb56d534ff22075db9f9bf95992
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2025-02-23 11:06:52 +0000

    first commit

--- index.js
+++ index.js
@@ -53,12 +53,20 @@ const Message = mongoose.model("Message", MessageSchema);
 
 // Signup Route
 app.post("/signup", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ username, email, password: hashedPassword });
-  await user.save();
-  res.status(201).json({ message: "User created successfully" });
-  console.log("User created successfully");
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+    console.error(error);
+  }
 });
 
 // Login Route
