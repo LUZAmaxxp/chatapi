bca3c3ec927598ecadb82a8ab23fafa54fdd44c1
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2025-02-27 11:32:16 +0000

    first commit

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ app.post("/api/login", async (req, res) => {
 app.get("/api/user-profile", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.userId).select(
-      "-password -friendRequests"
+      "-username -profilePic "
     );
 
     if (!user) {
