c3050f74560f8eb79fcd0f5ae659dfe1f838a33d
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2025-03-01 11:06:36 +0000

    Add uploads folder

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken";
+
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) throw new Error("No token provided");
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
@@ -9,4 +12,5 @@ const auth = async (req, res, next) => {
     res.status(401).json({ error: "Authentication required" });
   }
 };
-module.exports = auth;
+
+export default auth;
